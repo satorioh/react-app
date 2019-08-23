aeb2666de206ea9f648cd51c7c8a11d0fd2dabe9
satorioh <wangbinxp@gmail.com>
2019-08-23 16:50:32 +0800

    jenkins test7

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,50 +1,14 @@
-import React, { Component,Fragment } from 'react';
+import React, { Component } from 'react';
 import './style.css'
-import TodoItem from './TodoItem'
 
 class TodoList extends Component {
   constructor(props){
-    super(props)
-    this.state = {
-      inputValue:'',
-      list:[],
-    }
+    super(props);
+    this.state = {}
   }
 
     render() {
-    return (
-      <Fragment>
-        <div>hello world!!</div>
-      </Fragment>
-    );
-  }
-
-  getTodoItem(){
-    return this.state.list.map((item,index)=>{
-      return <TodoItem key={index} content={item} index={index} deleteItem={this.handleDelete}/>
-    })
-  }
-
-  handleInputValue = (e) => {
-    this.setState({
-      inputValue:e.target.value
-    })
-  }
-
-  handleClick = () => {
-    this.setState({
-      list:[...this.state.list, this.state.inputValue],
-      inputValue:''
-    })
-  }
-
-  handleDelete = (index) =>{
-    console.log(index)
-    const list = [...this.state.list]
-    list.splice(index, 1)
-    this.setState({
-      list:list
-    })
+        return <div>hello Gaia!!!!!</div>;
   }
 }
 
