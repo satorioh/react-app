dcd6d313ba51212d819d6faac69873f46174107c
satorioh <wangbinxp@gmail.com>
2019-09-27 09:37:19 +0800

    add develop branch

--- src/TodoList.js
+++ src/TodoList.js
@@ -209,7 +209,7 @@ class TodoList extends Component {
 
     render() {
         console.log(this.nestedToFlat(this.state.units))
-        return <div>release</div>;
+        return <div>develop</div>;
   }
 
     nestedToFlat = (children) => {
