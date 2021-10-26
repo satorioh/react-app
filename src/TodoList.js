270ec305adee0a0ae164e629d7654a6211352cfe
satorioh <wangbinxp@gmail.com>
2021-10-26 17:09:03 +0800

    hotfix test

--- src/TodoList.js
+++ src/TodoList.js
@@ -209,7 +209,7 @@ class TodoList extends Component {
 
     render() {
         console.log(this.nestedToFlat(this.state.units))
-        return <div>feature test</div>;
+        return <div>hotfix test</div>;
   }
 
     nestedToFlat = (children) => {
